54807fbba2ce2dc5a83c6da2f9f6590ab9beae1a
Turvenuija <132452863+turvenuija1@users.noreply.github.com>
2023-06-14 13:51:24 +0300

    Update Aboutme.js

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -8,7 +8,7 @@ function AboutMe() {
             <h3>About me & socials</h3>
             <p>on my sigma grindset 💯</p>
             <p>i like coding, especially making websites</p>
-            <p>my discord: Turvenuija#0154</p>
+            <p>my discord: </>#0154</p>
             <p>my email: turvenuija@skidd.xyz</p>
             <p>my github: /turvenuija1</p>
 
