5a3c29e03b1eb39f014d423256c0d62d0218dbf4
Turvenuija <132452863+turvenuija1@users.noreply.github.com>
2023-07-09 21:02:54 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+import Main from './Main.jsx';
+
+
+const main = ReactDOM.createRoot(document.getElementById("main"));
+main.render(<Main />);
+
